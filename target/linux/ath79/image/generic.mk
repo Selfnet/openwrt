7c5e920822442883fbfff21c2ddf47c8dfd973c4
Marco von Rosenberg <codingmarco@gmail.com>
2023-09-08 23:22:47 +0200

    generic.mk: Pad rootfs to minimum required size, otherwise u-boot will not flash it

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -290,9 +290,10 @@ define Device/huawei_ap5030dn
   DEVICE_VENDOR := Huawei
   DEVICE_MODEL := AP5030DN
   DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct
+  ROOTFS_MIN_SIZE := 10240k
   IMAGES := kernel.bin squashfs.bin
   IMAGE/kernel.bin := | append-kernel
-  IMAGE/squashfs.bin := append-rootfs | pad-to $$$$(BLOCKSIZE)
+  IMAGE/squashfs.bin := append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_MIN_SIZE) | pad-to $$$$(BLOCKSIZE)
 endef
 TARGET_DEVICES += huawei_ap5030dn
 
