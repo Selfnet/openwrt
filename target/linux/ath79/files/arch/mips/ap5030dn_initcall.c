837bcd261ac42fc4086a01e66eac579b767938db
Marco von Rosenberg <codingmarco@gmail.com>
2024-03-18 19:49:27 +0100

    Add patch to backport-6.1 since it is now upstream in 6.8

---
+++ target/linux/ath79/files/arch/mips/ap5030dn_initcall.c
@@ -0,0 +1,8 @@
+#include <linux/kernel.h>
+
+static void __init ap5030dn_test_toggle_gpio(void)
+{
+    printk(KERN_INFO "AP5030DN WDT\n");
+    asdfasdf
+}
+early_initcall(ap5030dn_test_toggle_gpio);
